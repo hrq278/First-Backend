2cd84582b3fce013dd2c35e6730e9c0a53145956
Haroon Rasheed <hrq27@gmail.com>
2025-05-28 17:13:21 +0500

    add comment, like, and playlist models; update tweet model; fix user route for avatar and cover image uploads

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -169,8 +169,12 @@ const logoutUser = asyncHandler( async(req, res)=>{
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set : {
-                refreshToken : undefined
+            // $set : {
+            //     refreshToken : undefined
+            // }
+            $unset:{
+                refreshToken:1
+                //this removes the filed from document
             }
         },
         {
